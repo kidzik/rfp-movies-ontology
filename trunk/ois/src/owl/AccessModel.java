afc416730e16eb755bce4e95f99fa9ad28e0b3b0
wolneylmn@gmail.com <wolneylmn@gmail.com@6c778c6b-255e-eed4-4825-8a64e4162094>
2011-06-02 16:56:35 +0000

    eclipse project cleaned

--- trunk/ois/src/owl/AccessModel.java
+++ trunk/ois/src/owl/AccessModel.java
@@ -1,4 +1,4 @@
-package movie;
+package owl;
 
 import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.util.FileManager;
--- trunk/ois/src/owl/SPARQLQueries.java
+++ trunk/ois/src/owl/SPARQLQueries.java
@@ -1,4 +1,4 @@
-package movie;
+package owl;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
